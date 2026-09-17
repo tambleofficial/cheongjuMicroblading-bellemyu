5b6d28427885866d919fafa431b9d3fec661bac9
tambleofficial <tambleofficial@gmail.com>
2026-09-17 21:37:35 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+document.querySelectorAll('a[href^="#"]').forEach(function(link){link.addEventListener('click',function(event){var target=document.querySelector(link.getAttribute('href'));if(target){event.preventDefault();target.scrollIntoView({behavior:'smooth',block:'start'})}})});
